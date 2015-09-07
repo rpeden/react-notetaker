0ee6c25cdf79ef81e9bf50c9889208b2683906fc
Ryan Peden <ryan04@gmail.com>
2015-09-07 18:13:13 -0400

    added NotesList component

---
+++ app/components/Notes/NotesList.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react'
+
+const NotesList = React.createClass({
+  render () {
+    const notes = this.props.notes.map( (note,index) => {
+      return <li className="list-group-item" key={index}> {note} </li>
+    });
+
+    return (
+      <ul className="list-group">
+        {notes}
+      </ul>
+    )
+  }
+})
+
+export default NotesList;
