18750ee068ef1121c2ddb86b948194f7ab790c4c
Ryan Peden <ryan04@gmail.com>
2015-09-07 18:13:27 -0400

    added AddNote component

---
+++ app/components/Notes/AddNote.jsx
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react'
+
+export default class AddNote extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleSubmit () {
+    const newNote = this.refs.note.getDOMNode().value;
+    this.refs.note.getDOMNode().value = '';
+    this.props.addNote(newNote);
+  }
+
+  render () {
+    return (
+      <div className="input-group">
+        <input type="text" className="form-control" ref="note" placeholder="Add New Note"></input>
+        <span className="input-group-btn">
+          <button className="btn btn-default" type="button" onClick={this.handleSubmit}> Submit </button>
+        </span>
+      </div>
+    )
+  }
+}
+
+AddNote.propTypes = {
+  addNote: React.PropTypes.func.isRequired
+};
