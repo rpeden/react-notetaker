5e2d34b814fb00cd88404fc73f9b027152b30cdd
Ryan Peden <ryan04@gmail.com>
2015-09-07 21:44:31 -0400

    updated main to add search component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,12 +1,13 @@
-var React = require('react');
-var RouteHandler = require('react-router').RouteHandler;
+import React from 'react'
+import SearchGithub from './Notes/SearchGithub.jsx'
+import { RouteHandler } from 'react-router';
 
 var Main = React.createClass({
     render: function() {
         return (<div className="main-container">
                     <nav className="navbar navbar-default" role="navigation">
                         <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-                            MENU
+                            <SearchGithub />
                         </div>
                     </nav>
                     <div className="container">
@@ -15,5 +16,5 @@ var Main = React.createClass({
                 </div>)
     }
 });
-        
+
 module.exports = Main;
