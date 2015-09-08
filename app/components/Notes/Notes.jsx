6cd83c27b6611bdb79f6069bf29170736dd82fa0
Ryan Peden <ryan04@gmail.com>
2015-09-07 23:18:43 -0400

    Notes ES6ify

--- app/components/Notes/Notes.jsx
+++ app/components/Notes/Notes.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import NotesList from './NotesList.jsx';
 import AddNotes from './AddNote.jsx';
 
-const Notes = React.createClass({
-  propTypes: {
-    username: React.PropTypes.string.isRequired,
-    notes: React.PropTypes.array.isRequired,
-    addNote: React.PropTypes.func.isRequired
-  },
-  render: function() {
-    const noteRows = [];
+class Notes extends React.Component {
 
-    this.props.notes.forEach(function(note){
-      noteRows.push(<div>{note}</div>);
-    });
+  static propTypes = {
+   username: React.PropTypes.string.isRequired,
+   notes: React.PropTypes.array.isRequired,
+   addNote: React.PropTypes.func.isRequired
+  }
 
-    let notes = (
+  render() {
+    return (
       <div>
         <h3>Notes for {this.props.username}</h3>
         <AddNotes addNote={this.props.addNote} />
@@ -24,8 +20,8 @@ const Notes = React.createClass({
         </div>
       </div>
     );
-    return notes;
   }
-});
 
-module.exports = Notes;
+}
+
+export default Notes;
