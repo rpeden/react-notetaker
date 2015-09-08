8c71e945dd42ea6269eac58e9991c0356f8cc736
Ryan Peden <ryan04@gmail.com>
2015-09-07 21:44:50 -0400

    finished repos component

--- app/components/Github/Repos.jsx
+++ app/components/Github/Repos.jsx
@@ -6,9 +6,20 @@ const Repos = React.createClass({
     repos: React.PropTypes.array.isRequired
   },
   render: function() {
+    const repos = this.props.repos.map((repo, index) => {
+      return (
+        <li className="list-group-item" key={index}>
+          {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+          {repo.description && <p> {repo.description} </p>}
+        </li>
+      )
+    })
     return (
       <div>
         <h3>Repos</h3>
+        <ul className="list-group">
+          {repos}
+        </ul>
       </div>
     );
   }
