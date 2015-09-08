4fb58bc51332d1c49f548b6a1c9a9eff0c7f8962
Ryan Peden <ryan04@gmail.com>
2015-09-07 23:37:44 -0400

    ES6ify Main component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import SearchGithub from './Notes/SearchGithub.jsx'
 import { RouteHandler } from 'react-router';
 
-var Main = React.createClass({
-    render: function() {
-        return (<div className="main-container">
-                    <nav className="navbar navbar-default" role="navigation">
-                        <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-                            <SearchGithub />
-                        </div>
-                    </nav>
-                    <div className="container">
-                        <RouteHandler />
-                    </div>
-                </div>)
-    }
-});
+class Main extends React.Component {
+  render() {
+      return (<div className="main-container">
+                  <nav className="navbar navbar-default" role="navigation">
+                      <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+                          <SearchGithub />
+                      </div>
+                  </nav>
+                  <div className="container">
+                      <RouteHandler />
+                  </div>
+              </div>)
+  }
+}
 
-module.exports = Main;
+export default Main;
