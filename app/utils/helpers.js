7d562e34310c264a5bc501c0172b51f7df474ac9
Ryan Peden <ryan04@gmail.com>
2015-09-07 21:43:38 -0400

    added helpers file

---
+++ app/utils/helpers.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const getRepos = (username) => {
+  return axios.get('https://api.github.com/users/' + username + '/repos');
+}
+
+const getUserInfo = (username) => {
+  return axios.get('https://api.github.com/users/' + username);
+}
+
+const helpers = {
+  getGithubInfo: function(username) {
+    return axios.all([getRepos(username), getUserInfo(username)])
+      .then( (arr) => {
+        return {
+          repos: arr[0].data,
+          bio: arr[1].data
+        }
+      })
+  }
+}
+
+export default helpers;
